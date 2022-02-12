f226c2295d04057a930c8979e7453b407d40d1b2
AlesandraIsla <alesandramempis@gmail.com>
2022-02-11 22:22:37 -0300

    Media query adicionado no scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import { useEffect, useState } from 'react';
-
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
-
 import { api } from './services/api';
-
 import './styles/global.scss';
-
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
@@ -23,9 +16,6 @@ export function App() {
 
   const [selectedGenreId, setSelectedGenreId] = useState(1);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-  
-
- 
 
   useEffect(() => {
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
@@ -38,7 +28,7 @@ export function App() {
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
+    <div className="principal">
       <SideBar selectedGenreId={selectedGenreId} handleClickButton={handleClickButton}/>
       <Content selectedGenre={selectedGenre} selectedGenreId={selectedGenreId}/>
     </div>
